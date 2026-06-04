fb740266dc778655710af29ea73f4cecd598bc78
MinhNC <decade69125@gmail.com>
2026-06-04 18:16:58 +0700

    chinh sua hien thi loi

--- test-cauthang.py
+++ test-cauthang.py
@@ -3,9 +3,9 @@ from ultralytics import YOLO
 import time
 from stair_monitor.analyzer import BehaviorAnalyzer
 from stair_monitor.rendering import (
-    draw_people_count,
     draw_person_overlay,
     draw_scene_guides,
+    draw_violation_summary,
     get_feet_point,
     get_motion_point,
 )
@@ -14,6 +14,7 @@ from stair_monitor.settings import (
     DRAW_DEBUG,
     SAVE_OUTPUT_VIDEO,
     SHOW_PEOPLE_COUNT,
+    VIOLATION_COUNT_LABELS,
     VIDEO_INPUT_PATH,
     VIDEO_OUTPUT_PATH,
     load_camera_config,
@@ -34,13 +35,21 @@ def process_video():
             25,
             (int(cap.get(3)), int(cap.get(4))),
         )
+    total_violation_track_ids = set()
+    total_violation_by_type = {
+        label: set() for label in VIOLATION_COUNT_LABELS
+    }
     start_time = time.time()
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
-        inside_track_ids = set()
+        current_inside_track_ids = set()
+        current_violation_track_ids = set()
+        current_violation_counts = {
+            label: 0 for label in VIOLATION_COUNT_LABELS
+        }
         yolo_frame = frame.copy()
         debug_frame = frame.copy()
 
@@ -64,14 +73,42 @@ def process_video():
             ):
                 p_lane = get_feet_point(box, kpt)
                 p_motion = get_motion_point(box, kpt)
-                if tid is not None and analyzer.is_inside_stairs(p_lane):
-                    inside_track_ids.add(int(tid))
                 ana = analyzer.analyze(tid, p_lane, p_motion, kpt)
+                track_id = ana.get("track_id", tid)
+                if ana.get("inside_stairs") and track_id is not None:
+                    current_inside_track_ids.add(int(track_id))
+
+                warnings = ana.get("warnings", [])
+                real_warnings = [
+                    warning
+                    for warning in warnings
+                    if warning in VIOLATION_COUNT_LABELS
+                ]
+                if real_warnings and track_id is not None:
+                    current_violation_track_ids.add(int(track_id))
+                    total_violation_track_ids.add(int(track_id))
+                    for warning in real_warnings:
+                        current_violation_counts[warning] += 1
+                        total_violation_by_type[warning].add(int(track_id))
+
                 draw_person_overlay(debug_frame, box, kpt, p_lane, p_motion, ana)
 
-        current_inside_count = len(inside_track_ids)
+        current_inside_count = len(current_inside_track_ids)
+        current_violation_people_count = len(current_violation_track_ids)
+        total_violation_people_count = len(total_violation_track_ids)
+        total_violation_counts = {
+            label: len(track_ids)
+            for label, track_ids in total_violation_by_type.items()
+        }
         if SHOW_PEOPLE_COUNT:
-            draw_people_count(debug_frame, current_inside_count)
+            draw_violation_summary(
+                debug_frame,
+                current_inside_count,
+                current_violation_people_count,
+                current_violation_counts,
+                total_violation_people_count,
+                total_violation_counts,
+            )
 
         if SAVE_OUTPUT_VIDEO and out is not None:
             out.write(debug_frame)
